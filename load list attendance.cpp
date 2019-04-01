872a8470b7dc4d3e60d84c88f22d34735774a250
Hoàng Xuân Kiệt <48423316+18127124@users.noreply.github.com>
2019-04-01 14:32:52 +0700

    Update load list attendance.cpp

--- load list attendance.cpp
+++ load list attendance.cpp
@@ -49,11 +49,66 @@ bool loadattendance(const char*file, nodeat *l1, char b[])
 
 		insertTail(l1, a);
 	}
+	return true;
+}
+struct attth {
+	char id[100];
+	int a[11];
+};
+struct nodeatth {
+	attth data;
+	nodeatth*next;
+};
+nodeatth*createnodeatth(attth x)
+{
+	nodeatth *p = new nodeatth;
+	p->next = NULL;
+	p->data = x;
+	return p;
+}
+void insertTail(nodeatth *&head, attth a)
+{
+	nodeatth *blank = createnodeatth(a);
+	nodeatth *current = new nodeatth;
+	if (head == NULL)
+	{
+		head = blank;
+	}
+	else
+	{
+		current = head;
+		while (current->next != NULL)
+			current = current->next;
+		current->next = blank;
+	}
+}
+
+bool loadattendanceth(const char*file, nodeatth *l1th, char b[])
+{
+	ifstream fin;
+	fin.open(file);
+	if (fin.fail())
+		return false;
+	fin.getline(b, 200, '\n');
+	attth a;
+	while (!fin.eof())
+	{
+		fin.getline(a.id, 10, ',');
+		for (int i = 0; i < 10; i++)
+		{
+			fin >> a.a[i];
+			fin.get();
+		}
+
+
+		insertTail(l1th, a);
+	}
+	return true;
 }
 void main()
 {
-	nodeat *l1 = new nodeat;
+	nodeatth *l1 = new nodeatth;
 	l1 = NULL;
 	char b[200];
-	loadattendance("18CLC1-CTT003.csv", l1, b);
+	loadattendanceth("18CLC1-CTT003th.csv", l1, b);
 }
