1654ab770dbb64aa5399e5ba211a5e1523cbe874
PhuongAria <48446995+PhuongAria@users.noreply.github.com>
2019-04-02 20:39:14 +0700

    Add files via upload

---
+++ YC28.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+bool viewcourse(listcr &lcr, char account[] ) //account của gv đăng nhập
+{
+	Nodecr* p=lcr.head;
+	while (p)
+	{
+		char *q=strstr(p.lecture,account);
+		if (p)
+		{
+			cout<<p.no<<'.'<<p.id<<','<<p.name<<','<<p.classn<<','<<p.lecture<<
+				p.start.day<<','<<p.start.month<<','<<p.start.year<<','<<
+				p.end.day<<','<<p.end.month<<','<<p.end.year<<','<<
+				p.start1.hour<<','<<p.start1.minute<<','<<
+				p.end1.hour<<','<<p.end1.minute<<','<<p.room<<endl;
+
+		}
+		p=p->next;
+	}
+}
